9107d0321d548ac0f93860dede6fbaee48434957
DGunnels <45681663+DGunnels@users.noreply.github.com>
2018-12-07 01:16:47 -0700

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,21 @@
+#ifndef PA3_FUNCTIONS_H
+#define PA3_FUNCTIONS_H
+// functions.h
+/*
+ * readFile function
+ * printDealers
+ * printCars
+ * chooseDealCarDisplay
+ * chooseDealCarAdd
+ * chooseDealCarMod
+ * chooseDealVINSort
+ * writeFile
+ * programExit
+ */
+
+
+
+
+
+
+#endif //PA3_FUNCTIONS_H
